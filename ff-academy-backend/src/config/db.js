f5e9c91e35693b0a9e8b606dec9e50f34f865939
Zoltan Zseli <zzseli@cloudera.com>
2025-12-02 14:49:13 +0100

    Pipeline fix #2

--- ff-academy-backend/src/config/db.js
+++ ff-academy-backend/src/config/db.js
@@ -8,17 +8,24 @@ const connectDB = async () => {
             MONGO_PASSWORD,
             MONGO_DATABASE,
             MONGO_AUTHFROM,
+            AWS
         } = process.env;
 
-        const uri = `${MONGO_SERVER}:27017/${MONGO_DATABASE}`;
-
-        const conn = await mongoose.connect(uri, {
+        let uri = `${MONGO_SERVER}:27017/${MONGO_DATABASE}`;
+        let options = {
             user: MONGO_USERNAME,
             pass: MONGO_PASSWORD,
             authSource: MONGO_AUTHFROM,
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        }
+
+        if (AWS) {
+            uri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_SERVER}/?appName=${MONGO_DATABASE}`
+            options = {};
+        }
+        
+        const conn = await mongoose.connect(uri, options);
 
         console.log(`✅ MongoDB Connected: ${conn.connection.host}/${MONGO_DATABASE}`);
     } catch (error) {
