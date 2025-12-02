9b324d49355c1bef8161068b9bff3b67f5821694
Zoltan Zseli <zzseli@cloudera.com>
2025-12-02 15:30:43 +0100

    Env variables fix for aws

--- ff-academy-frontend/src/environments/environment.ts
+++ ff-academy-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     tokenHandling: false,
     production: false,
-    API_BASE_PATH: 'http://localhost:4202',
+    API_BASE_PATH: 'http://ffacademy.us-east-1.elasticbeanstalk.com',
     DEV_API_BASE_PATH: 'http://localhost:4202',
 };
